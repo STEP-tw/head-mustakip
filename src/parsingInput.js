07f8840f57917e3d9ed465d9e3c33d34370662ed
MUSTAKIM PATVEKAR <44019216+mustakip@users.noreply.github.com>
2018-12-06 22:36:35 +0530

    Refactor extractArgs function and remove validateType and getParameters function

--- src/parsingInput.js
+++ src/parsingInput.js
@@ -1,41 +1,24 @@
 const {identity,doesIncludeNumber} = require("./util.js");
 
-const validateType = function(list) {
-  if(doesIncludeNumber(list[0]) && list[0].startsWith("-")) {
-    if(!list[0].startsWith("-n")){
-      list.unshift("-n");
-    }
-  }
-  if(!list[0].startsWith("-")) {
-    list.unshift("-n10");
-  }
-  return list;
-}
-
-const getParameters = function(list) {
+const extractArgs = function(argsList) {
+  argsList = argsList.slice(2);
   let type = "n";
-  if(list[0].includes("c")) {
+  if(argsList[0].includes("-c")) {
     type = "c";
   };
   let headType = type;;
-  let numberOfLines = list[0].split(type).reverse()[0];
-  numberOfLines = +numberOfLines.split("-").reverse()[0];
-  let files = list.slice(1,list.length);
-  return {headType,numberOfLines,files}
+  let numberOfLines = 10;
+  let files = argsList;
+  if(doesIncludeNumber(argsList[0]) && argsList[0].startsWith("-")) {
+    numberOfLines = argsList[0].split(type).reverse()[0];
+    numberOfLines = +numberOfLines.split("-").reverse()[0];
+    files = argsList.slice(1);
+  }
+  if(!doesIncludeNumber(argsList[0]) && argsList[0].startsWith("-")) {
+    numberOfLines = argsList[1];
+    files = argsList.slice(2);
+  }
+  return {headType,numberOfLines,files} 
 }
 
-const extractArgs = function(argsList) {
-  let list = identity(argsList).slice(2,argsList.length);
-  list = validateType(list);
-  if(!doesIncludeNumber(list[0])) {
-    list[0] = list[0]+list[1];
-    list.splice(1,1);
-    return getParameters(list);
-  }
-  return getParameters(list);
-}
-module.exports = { 
-  extractArgs,
-  getParameters,
-  validateType
-};
+module.exports = {extractArgs};
--- test/parsingInputTest.js
+++ test/parsingInputTest.js
@@ -1,5 +1,5 @@
 const {deepEqual} = require("assert");
-const {getParameters,extractArgs,validateType} = require("../src/parsingInput.js");
+const {extractArgs} = require("../src/parsingInput.js");
 
 describe('Test for parsingInput.js', function () {
 
@@ -75,42 +75,6 @@ describe('Test for parsingInput.js', function () {
       expectedOutput = {headType : "c", files : ["file1","file2"], numberOfLines : 5};
 
       deepEqual(extractArgs(argsList),expectedOutput);
-    });
-  }); 
-
-  describe('Test validateType function', function () {
-    it('it should return type and number of lines as 10 default when not provided', function () {
-      let argsList = ["file.js"];
-      let expectedOutput = ["-n10","file.js"];
-      deepEqual(validateType(argsList),expectedOutput);
-    });
-    it('it should return type -n as first element when number of lines is provided', function () {
-      let argsList = ["-5","file1.js"];
-      let expectedOutput = ["-n","-5","file1.js"];
-      deepEqual(validateType(argsList),expectedOutput);
-    });
-    it('it should return type -n5 as first element when number of lines is provided with type', function () {
-      let argsList = ["-n5","file1.js"];
-      let expectedOutput = ["-n5","file1.js"];
-      deepEqual(validateType(argsList),expectedOutput);
-    });
-  }); 
-
-  describe('Test getParameters', function () {
-    it('should return headType,numberOfLines and files array in object(single file,-n-5)', function () {
-      let argsList = ["-n-5","file1.js"];
-      expectedOutput = {headType : "n",numberOfLines : 5, files :["file1.js"]};
-      deepEqual(getParameters(argsList),expectedOutput);
-    });
-    it('should return headType,numberOfLines and files array in object(multiple files)', function () {
-      let argsList = ["-n-5","file1.js","file2.js"];
-      expectedOutput = {headType : "n",numberOfLines : 5, files :["file1.js","file2.js"]};
-      deepEqual(getParameters(argsList),expectedOutput);
-    });
-    it('should return headType,numberOfLines and files array in object(multiple files,-n5)', function () {
-      let argsList = ["-n10","file1.js"];
-      expectedOutput = {headType : "n",numberOfLines : 10, files :["file1.js"]};
-      deepEqual(getParameters(argsList),expectedOutput);
     });
   }); 
 });
