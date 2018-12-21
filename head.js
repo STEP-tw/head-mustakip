d506af6078c05fb4e8e8146ad0f95e4fb6cc7d9a
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2018-12-21 11:16:16 +0530

    Remove unnecessary code from head.js

--- head.js
+++ head.js
@@ -4,7 +4,6 @@ const {parseInput} = require("./src/library/parseInput.js");
 
 const main = function() {
   let args = parseInput(process.argv.slice(2));
-  args.type = "head";
   console.log(head(fs, args));
 };
 
